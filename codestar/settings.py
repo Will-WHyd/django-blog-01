32a1acdc806394d6276f7115d8b815f8c39a616d
William Waldron-Hayden <williamjwh@gmail.com>
2024-11-25 13:07:15 +0000

    enable serving of image files

--- codestar/settings.py
+++ codestar/settings.py
@@ -31,7 +31,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-willwhyd-djangoblog01-0so900tk24d.ws.codeinstitute-ide.net',
                 '8000-willwhyd-djangoblog01-fqxws7twq65.ws.codeinstitute-ide.net',
@@ -47,11 +47,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'cloudinary_storage',
     'django.contrib.sites',
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
     'django_summernote',
+    'cloudinary',
     'blog',
     'about',
     'crispy_forms',
